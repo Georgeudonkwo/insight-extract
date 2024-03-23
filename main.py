b740bed5123f6fd74a62d19b27f786275a2e1aaa
Georgeudonkwo <enogeorgeudonkwo@gmail.com>
2024-03-23 15:00:49 +0100

    added the fetch.py module

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
 import streamlit as st
 import time
 from src.sourcefiles import analyse_documents,load_wiki
+from src.fetch import fetch_agent
+from src.llm_models import google_models
 st.title('Document Analyser')
 cols=st.columns([0.4,0.6])
 doc=None
+llm=None
+database_object=None
 with st.sidebar:
     path=''
-    st.sidebar.selectbox('INPUT MODE',['files','search'],key='select')
+    st.sidebar.selectbox('Data Context',['files','Database','wikipedia'],key='select')
     if st.session_state.select=='files':
         st.sidebar.file_uploader('get file path by copying')
         path= st.text_area('Enter file path',placeholder="file name")
         st.write(path)
-    elif st.session_state.select=='search':
+    elif st.session_state.select=='wikipedia':
+        path= st.text_area('Enter search token',placeholder="Search wikipedia")
+        st.write(path)
+    elif st.session_state.select=='Database':
+        llm=google_models()
+        dialet=st.text_input("Database Type",value='mysql')
+        if dialet.strip()=='mysql':
+            engine=st.text_input('dialetr',value='mysqlconnector')
+            user=st.text_input('user',value='George')
+            password=st.text_input('password',value='georgedb*george1')
+            host=st.text_input('host',value='CCLNG-PC310')
+            port=st.text_input('port',value=3306)
+            db_name=st.text_input('database name',value='MISC')
+            connstr=f"{dialet}+{engine}://{user}:{password}@{host}:{port}/{db_name}"
+        elif  dialet.strip()=='mssql':
+            pass
+        elif dialet.strip()=='mssql':
+            dialet=st.text_input("Database Type",value='sqlite')
+            filepath=st.text_area('file path',value=R"C:\Users\eno.udonkwo\Desktop\2024 works\data\misc.db")
+            connstr=f"{dialet}:///"+filepath
+        
+            
+
+
         path= st.text_area('Enter search token',placeholder="Search")
         st.write(path)
 if st.session_state.select=='files':
@@ -22,21 +49,34 @@ else:
 with st.form('chat_form'):
     with cols[0]:
         st.header(':red[Insight Extraction]')
-    query = st.text_area('What do you want to do :question:','summarise the uploaded document!')
+    if st.session_state.select=='files':
+        query = st.text_area('What do you want to do :question:','summarise the uploaded document!')
+    elif st.session_state.select=='Database':
+        query=st.text_area('interact with your database',value='fetch table info')
     submitted = st.form_submit_button(':blue[Submit Request] :running:')
     if submitted:
-        with st.spinner(spintext):
-            starttime=time.time()
-            chain,doc=analyse_documents(path)
-            res=chain.invoke(query)
-            endtime=time.time()
-            st.write(f' the inference ran for {round((endtime-starttime),2)} seconds')
-            st.info(res)
-        with cols[1]:
-            with st.expander(':green[SEE SOURCE DOCUMENT]'):
-                st.header('Source')
-                if st.session_state.select=='search':
-                    text=doc
-                else:
-                    text="".join([d.page_content for d in doc])
-                st.write(text)
+        if st.session_state.select=='files':
+            with st.spinner(spintext):
+                starttime=time.time()
+                chain,doc=analyse_documents(path)
+                res=chain.invoke(query)
+                endtime=time.time()
+                st.write(f' the inference ran for {round((endtime-starttime),2)} seconds')
+                st.info(res)
+            with cols[1]:
+                with st.expander(':green[SEE SOURCE DOCUMENT]'):
+                    st.header('Source')
+                    if st.session_state.select=='wikipedia':
+                        text=doc
+                    else:
+                        text="".join([d.page_content for d in doc])
+                    st.write(text)
+        elif st.session_state.select=='Database':
+            spintext='connecting to the database, this may take awhile. please wait ...:raised_hand_with_fingers_splayed:'
+            with st.spinner(spintext):
+                starttime=time.time()
+                database_object=fetch_agent(connString=connstr,llm=llm,query=query)
+                endtime=time.time()
+                st.write(f' the inference ran for {round((endtime-starttime),2)} seconds')
+                st.info(database_object)
+        
--- src/fetch.py
+++ src/fetch.py
@@ -2,6 +2,8 @@ from langchain_core.prompts.base import BasePromptTemplate
 from langchain_core.prompts.prompt import PromptTemplate
 from langchain_core.language_models.base import BaseLanguageModel
 from langchain_community.utilities.sql_database import SQLDatabase
+from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
+from langchain_community.agent_toolkits.sql.base import create_sql_agent
 from langchain.chains.sql_database.query import create_sql_query_chain
 from langchain_core.runnables import RunnablePassthrough,RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
@@ -13,7 +15,16 @@ def db_instance(connString:str):
     table_info=db.get_table_info()
     context=db.get_context()
     return db,table_info,table_names,context
+def fetch_agent(connString:str,llm:BaseLanguageModel,query:str):
+    db,tbl_info,tbl_names,contxt=db_instance(connString=connString)
+    db_tool_kit=SQLDatabaseToolkit(db=db,llm=llm)
+    agent=create_sql_agent(llm=llm,toolkit=db_tool_kit
+                           ,agent_type='zero-shot-react-description',
+                           verbose=True)
+    result=agent.run(query)
+    return result
 
+    
 def fetch(connString:str,llm:BaseLanguageModel):
     db,tbl_info,tbl_names,contxt=db_instance(connString=connString)
     sqlprompt=PromptTemplate.from_template(template='''you are an expert database administrator,
@@ -37,9 +48,19 @@ def fetch(connString:str,llm:BaseLanguageModel):
            #|StrOutputParser()
           # )
     return res
+def get_mysql_connection_string(user:str='George',psw:str='georgedb*george1',
+                    host:str='CCLNG-PC310',dbname:str='MISC',port=3306):
+    dialet='mysql'
+    engine='mysqlconnector'
+    connstr=f"{dialet}+{engine}://{user}:{psw}@{host}:{port}/{dbname}"
+    return connstr
 
 if __name__=='__main__':
     llm=google_models()
-    connstr=R"sqlite:///C:\Users\eno.udonkwo\Desktop\2024 works\data\misc.db"
+    #connstr=R"sqlite:///C:\Users\eno.udonkwo\Desktop\2024 works\data\misc.db"
+    #connstr=get_mysql_connection_string()
+    connstr=R"sqlite:///C:\Users\eno.udonkwo\Desktop\2024 works\data\PVTRMS-mod.db"
     ch=fetch(connString=connstr,llm=llm)
+    query="how many tables in the datbase"
+    ch=fetch_agent(connString=connstr,llm=llm,query=query)
     print(ch)#.invoke({'input':"what is Joanna's surname"}))
